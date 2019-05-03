1225f5047ccdf9c480199e9baf06f73026c51b10
fjrd <alexander_reztsov@mail.ru>
2019-05-03 21:10:51 +0300

    bubble sorting

--- src/homework/lesson03/bubbleSort/bubbleSort.java
+++ src/homework/lesson03/bubbleSort/bubbleSort.java
@@ -1,7 +1,43 @@
 package homework.lesson03.bubbleSort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class bubbleSort {
 
+    static int[] fillArrayWithRandomValues(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1 + new Random().nextInt(arr.length);
+        }
+        return arr;
+    }
 
+    static void printArray(int arr[]){
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //bubble sorting
+    static void sortArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) { //{5, 2, 4, 9, 1}
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[15];
+        fillArrayWithRandomValues(arr);
+        System.out.println("Generated unsorted array:");
+        printArray(arr);
+        sortArray(arr);
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
 
 }
